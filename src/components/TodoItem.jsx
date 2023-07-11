ed3bad7fb0d87dfda2bc01f64800ded4bec13927
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-07-11 19:20:12 +0600

    Update todoitem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,21 +1,33 @@
 import PropTypes from 'prop-types';
 import styles from 'styles/TodoItem.module.css';
+// import { useState } from 'react';
 
-const TodoItem = ({ itemProp, handleChange, delTodo }) => (
-  <li className={styles.item}>
-    <div className={styles.content}>
-      <input
-        type="checkbox"
-        checked={itemProp.completed}
-        onChange={() => handleChange(itemProp.id)}
-      />
-      <button type="button" onClick={() => delTodo(itemProp.id)}>
-        Delete
-      </button>
-      {itemProp.title}
-    </div>
-  </li>
-);
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+  return (
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button type="button">Edit</button>
+        <button type="button" onClick={() => delTodo(itemProp.id)}>
+          Delete
+        </button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
+    </li>
+  );
+};
 
 TodoItem.propTypes = {
   itemProp: PropTypes.arrayOf(
